32cbcd2b8efdee2a2bcada55bace34400da4b33b
Bandari0228 <88103590+Bandari0228@users.noreply.github.com>
2024-03-11 17:23:37 +0530

    Add files via upload

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,6 +4,11 @@ import Homepage from "./components/home";
 import Login from "./components/login";
 import Signup from "./components/signup";
 import Profile from "./components/profile"
+import Library from "./components/library"
+import Navbar from "./components/navbar"
+import Favorites from "./components/favorites"
+import Aboutus from "./components/aboutus";
+import UserProfile from "./components/userprofile";
 function App() {
   return (
     <>
@@ -14,6 +19,12 @@ function App() {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/profile" element={<Profile/>} />
+        <Route exact path="/library" element={<Library/>} />
+        <Route exact path="/navbar" element={<Navbar/>} />
+        <Route exact path="/favorites" element={<Favorites/>} />
+        <Route exact path="/about" element={<Aboutus/>} />    
+        <Route exact path="/userprofile" element={<UserProfile/>} />    
+
 
       </Routes>
 
